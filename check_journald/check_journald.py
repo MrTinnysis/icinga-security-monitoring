b609608858d49bfbec087b70b285ad62f8acf41a
RAG-6\Raphael <raphael.gerber@ace.ch>
2019-05-15 09:43:01 +0200

    :trollface:

--- check_journald/check_journald.py
+++ check_journald/check_journald.py
@@ -9,6 +9,10 @@ def period(string):
         msg = "%r is not a valid period" % string
         raise argparse.ArgumentTypeError(msg)
     return string
+	
+def printPerformanceData(label, ctr, warn, crit):
+	data = ["%s=%s" % (label, str(ctr)), str(warn), str(crit)]
+	print(";".join(data))
 
 def main():
 	# monitoring plugin return codes
@@ -26,10 +30,10 @@ def main():
 		'-v', '--verbose', default=False,
 		help='verbose output')
 	argumentParser.add_argument(
-		'-w', '--warning', metavar='RANGE', default=range(0),
+		'-w', '--warning', metavar='RANGE', default=2,
 		help='return warning if count of found logs is outside RANGE')
 	argumentParser.add_argument(
-		'-c', '--critical', metavar='RANGE', default=range(0),
+		'-c', '--critical', metavar='RANGE', default=1,
 		help='return critical if count of found logs is outside RANGE')
 	argumentParser.add_argument(
 		'--hostname', metavar='HOST', default='localhost',
@@ -41,11 +45,14 @@ def main():
 		'--path',
 		help='path to journal log folder')
 	argumentParser.add_argument(
-		'--matches', nargs='+',
+		'-m', '--matches', nargs='+',
 		help='matches for logparse')
 	argumentParser.add_argument(
 		'--period', metavar='NUMBER', default='1h', type=period,
 		help='check log of last period (default: "1h", format 1-99 m/h/d)')
+	argumentParser.add_argument(
+		'-r', '--regex', help='Regular expression to match message content'
+	)
 
 	arguments = argumentParser.parse_args()
 
@@ -61,26 +68,53 @@ def main():
 		
 		journal.set_timeframe(arguments.period)
 		
+		if arguments.regex:
+			regex = re.compile(arguments.regex)
+			print(regex)
+			# filter journal by regex
+			journal = [entry for entry in journal if regex.search(entry["MESSAGE"])]
+			
+			if regex.groups == 1:
+				ctr = {}
+			else:
+				ctr = 0
+		else:
+			ctr = 0
+		
+		
 		#parse journal
-		ctr = 0
 		for entry in journal:
 			if arguments.verbose:
 				print(str(entry["__REALTIME_TIMESTAMP"]) + ": " + entry["MESSAGE"], end="\n")
-			ctr += 1
+			
+			if type(ctr) is dict:
+				match = regex.search(entry["MESSAGE"])
+				ctr.setdefault(match.group(1), 0)
+				ctr[match.group(1)] += 1
+			else:
+				ctr += 1
 		
-		
+		print(ctr)
 		returnCode = OK
-	
-		if ctr in arguments.warning:
-			returnCode = WARNING
+		
+		if type(ctr) is dict:
+			for key, val in ctr:
+				if val in range(arguments.warning, arguments.critical-1):
+					returnCode = WARNING
+				
+				if val >= arguments.critical:
+					returnCode = CRITICAL
 			
-			
-		if ctr in arguments.critical:
-			returnCode = CRITICAL
-			
-			
-		#TODO: Check Performance info format
-		print(ctr, end="\n")
+				printPerformanceData(key, val, arguments.warning, arguments.critical)
+		else:
+			if ctr in range(arguments.warning, arguments.critical-1):
+				returnCode = WARNING
+				
+				
+			if ctr >= arguments.critical:
+				returnCode = CRITICAL
+		
+			printPerformanceData("count", ctr, arguments.warning, arguments.critical)
 	else:
 		#TODO: Call remote plugin
 		pass
