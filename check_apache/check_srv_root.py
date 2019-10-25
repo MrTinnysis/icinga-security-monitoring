d710fac3d06f1750485ff37d6d3d09b139a3c95a
RAG-6\Raphael <raphael.gerber@ace.ch>
2019-10-25 13:56:32 +0200

    -

---
+++ check_apache/check_srv_root.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import argparse, sys, os
+
+
+def parse_args():
+    # Parses the CLI Arguments and returns a dict containing the 
+    # corresponding values
+    argumentParser = argparse.ArgumentParser()
+
+    argumentParser.add_argument(
+		'-v', '--verbose', nargs="?", const=True, default=False,
+		help='verbose output')
+    argumentParser.add_argument(
+		'--path', default="/usr/local/apache2",
+		help='path to the ServerRoot')
+
+    return argumentParser.parse_args()
+
+def process_stats(path, stats):
+    pass
+
+
+def main():
+    # Main Plugin Function
+
+    # monitoring plugin return codes
+    OK = 0
+    WARNING = 1
+    CRITICAL = 2
+    UNKNOWN = 3
+
+    # parse CLI Arguments
+    args = parse_args()
+
+    # Print CLI Arguments if verbose output is enabled
+    if args.verbose:
+        print(args)
+
+    if not os.path.isdir(args.path):
+        print("CRITICAL: The configured path does not denote a directory!")
+        sys.exit(CRITICAL)
+
+
+
+    for root, dirs, files in os.walk(args.path):
+        stats = os.stat(root)
+
+        if args.verbose:
+            print(f"Processing: {root}")
+            print(f"Stats: {stats}")
+
+        process_stats(root, stats)
+
+        for name in files:
+            path = os.path.join(root, name)
+            stats = os.stat(path)
+
+            if args.verbose:
+                print(f"Processing: {path}")
+                print(f"Status: {stats}")
+            
+            process_stats(path, stats)
+
+    
+    sys.exit(OK)
+
+
+if __name__=="__main__":
+	main()
--- check_apache/check_srv_token.py
+++ check_apache/check_srv_token.py
@@ -4,6 +4,8 @@ import argparse, sys, re
 
 
 def find_server_signature(httpd_conf):
+    # Searches a "ServerSignature" Configuration Entry and returns the 
+    # corresponding line, or None if no such Entry was found
     for line in httpd_conf:
         if "ServerSignature" in line:
             return line
@@ -11,6 +13,8 @@ def find_server_signature(httpd_conf):
 
 
 def find_server_tokens(httpd_conf):
+    # Searches a "ServerTokens" Configuration Entry and returns the
+    # corresponding line, or None if no such Entry was found
     for line in httpd_conf:
         if "ServerTokens" in line:
             return line
@@ -18,6 +22,8 @@ def find_server_tokens(httpd_conf):
 
 
 def parse_args():
+    # Parses the CLI Arguments and returns a dict containing the 
+    # corresponding values
     argumentParser = argparse.ArgumentParser()
 
     argumentParser.add_argument(
@@ -31,26 +37,33 @@ def parse_args():
 
 
 def main():
+    # Main Plugin Function
+
     # monitoring plugin return codes
     OK = 0
     WARNING = 1
     CRITICAL = 2
     UNKNOWN = 3
 
+    # parse CLI Arguments
     args = parse_args()
 
+    # Print CLI Arguments if verbose output is enabled
     if args.verbose:
         print(args)
 
     try:
+        # open the configuration file
         with open(args.path, "r") as httpd_conf:
-            # search entries in https_conf file
+            # search entries in httpd_conf file
             server_sig = find_server_signature(httpd_conf)
             server_tokens = find_server_tokens(httpd_conf)
     except FileNotFoundError:
+        # Abort if file could not be found
         print("CRITICAL: Config File Not Found")
         sys.exit(CRITICAL)
 
+    # print the configured entries 
     if args.verbose:
         print(f"server_sig = {server_sig}")
         print(f"server_tokens = {server_tokens}")
@@ -64,14 +77,19 @@ def main():
     match_sig = re.match("^ServerSignature (\w+)", server_sig)
     match_token = re.match("^ServerTokens (\w+)", server_tokens)
 
+    # Abort if configured values could not be retrieved
     if not match_sig  or not match_token:
         print("CRITICAL: Could not retrieve configured values")
         sys.exit(CRITICAL)
 
+    # Compare the configured values with the suggested values
     if match_sig.group(1) != "Off" or match_token.group(1) != "Prod":
         print("WARNING: Potentially insecure configuration for ServerSignature and/or ServerTokens")
+        print(f"Configured Value: {server_sig}, should be 'ServerSignature Off'")
+        print(f"Configured Value: {server_tokens}, should be 'ServerTokens Prod'")
         sys.exit(WARNING)
 
+    print("OK: Server Information hidden")
     sys.exit(OK)
 
 
