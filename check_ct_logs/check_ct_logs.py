c469c165684b9aa4741214bf6b7909eb0871478a
Louis Siegrist <louis.siegrist@gmx.net>
2019-10-28 11:47:17 +0100

    add ct check

---
+++ check_ct_logs/check_ct_logs.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import requests
+import argparse
+import sys
+import re
+
+
+def main():
+    # monitoring plugin return codes
+    OK = 0
+    WARNING = 1
+    CRITICAL = 2
+    UNKNOWN = 3
+
+    # defaults
+    returnCode = UNKNOWN
+    status = ''
+    performanceData = ''
+    warningCount = 0
+    criticalCount = 0
+
+    # parse arguments
+    argumentParser = argparse.ArgumentParser()
+
+    argumentParser.add_argument(
+        '--verbose', nargs='?', const=True, default=False,
+        help='verbose output')
+    argumentParser.add_argument(
+        '--domain', metavar='DOMAIN', type=str, required=True,
+        help='domain to search in certificate transparency logs')
+    argumentParser.add_argument(
+        '--issuer', metavar='ISSUER ORGANISATION NAME', nargs='+', type=str, required=True,
+        help='whitelist issuers')
+
+    arguments = argumentParser.parse_args()
+
+    try:
+
+        response = requests.get(
+            'https://api.certspotter.com/v1/issuances',
+            params=b'domain=bfh.ch&expand=dns_names&expand=issuer')
+
+        certCount = 0
+        for cert in response.json():
+            certCount += 1
+            issuerOrganisationName = re.findall('O=(.*),', cert['issuer']['name'])[0]
+            if issuerOrganisationName not in arguments.issuer:
+                criticalCount += 1
+                status += 'CRITICAL: ' + issuerOrganisationName + 'is not in whitelist!;'
+
+        if criticalCount > 0:
+            returnCode = CRITICAL
+        else:
+            returnCode = OK
+            status = 'OK: All found issuers are whitelisted.'
+
+        performanceData = ' | cert_count=' + str(certCount)
+
+    except Exception as e:
+        status = ('CRITICAL: ' + str(e))
+
+    print(status + performanceData)
+    sys.exit(returnCode)
+
+
+if __name__ == '__main__':
+    main()
