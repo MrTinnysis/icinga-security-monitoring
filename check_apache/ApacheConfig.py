21868d2b9b0a76064fb8050b2ac42d4b345393b8
RAG-6\Raphael <raphael.gerber@ace.ch>
2019-10-29 15:43:29 +0100

    -

--- check_apache/ApacheConfig.py
+++ check_apache/ApacheConfig.py
@@ -7,16 +7,19 @@ import apacheconfig
 
 
 def get_apache_config(path, options=None, env_vars=None, verbose=False):
-    """
 
-    """
+    if env_vars:
+        env_vars = _load_env_vars(env_vars, verbose)
+
 
     # set default options if none are given
     options = options if options != None else {
         "useapacheinclude": True,
         "includerelative": True,
         "includedirectories": True,
-        "configpath": [os.path.split(path)[0]]
+        "configpath": [os.path.split(path)[0]],
+        "flagbits": env_vars,
+        "interpolateenv": env_vars != None
     }
 
     # print options if verbose output is enabled
@@ -27,24 +30,18 @@ def get_apache_config(path, options=None, env_vars=None, verbose=False):
     with apacheconfig.make_loader(**options) as loader:
         config = loader.load(path)
 
-    # load environment variables if provided
-    if env_vars:
-        env_vars = _load_env_vars(env_vars, verbose)
+    # # load environment variables if provided
+    # if env_vars:
+    #     env_vars = _load_env_vars(env_vars, verbose)
 
-        regex = re.compile("$\{(.*)\}")
+    #     regex = re.compile("$\{(.*)\}")
 
-        for key, val in config.items():
-            if type(val) != str: continue
-            match = regex.match(val)
-            if match:
-                var = match.group(1)
-                config[key] = val.replace(f"${{{var}}}", env_vars[var])
-
-        # # replace placeholders in config values with env vars
-        # for key, val in config.items():
-        #     # TODO: Fix
-        #     config[key] = val.replace(f"${{{key}}}", env_vars[key])
-            # val = val.replace(f"${{{key}}}", env_vars[key])
+    #     for key, val in config.items():
+    #         if type(val) != str: continue
+    #         match = regex.match(val)
+    #         if match:
+    #             var = match.group(1)
+    #             config[key] = val.replace(f"${{{var}}}", env_vars[var])
 
     if verbose:
         print(config)
