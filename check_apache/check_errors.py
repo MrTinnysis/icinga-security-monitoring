08738d28101bcd042e8a080c616426a1190dc35f
RAG-6\Raphael <raphael.gerber@ace.ch>
2019-11-01 13:22:34 +0100

    -

--- check_apache/check_errors.py
+++ check_apache/check_errors.py
@@ -117,10 +117,7 @@ def main():
 
     # count entries per ip
     ip_list = groupby(sorted(log_data, key=lambda x: x["remote_host"]), key=lambda x: x["remote_host"])
-
-    print(ip_list)
-
-    ip_count = [len(x) for x in ip_list]
+    ip_count = [(x["remote_host"], len(x)) for x in ip_list]
 
     print(ip_count)
 
