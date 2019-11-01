62433a8d6a83f7b0c6037c38f8ee98d85fc16b16
RAG-6\Raphael <raphael.gerber@ace.ch>
2019-11-01 14:54:22 +0100

    -

--- check_apache/ApacheLogs.py
+++ check_apache/ApacheLogs.py
@@ -1,6 +1,7 @@
 
 import re
 import os
+import typing
 
 # https://github.com/rory/apache-log-parser
 # https://pypi.org/project/apache-log-parser/1.7.0/
@@ -15,7 +16,7 @@ class ApacheLogParserException(Exception):
 
 class ApacheLogParser:
 
-    def __init__(self:ApacheLogParser, config: ApacheConfig, vhost:str=None) -> ApacheLogParser:
+    def __init__(self, config:ApacheConfig, vhost:str=None) -> ApacheLogParser:
         log_format_list = config.get("LogFormat", vhost=vhost)
         custom_log = config.get("CustomLog", vhost=vhost)
 
@@ -54,11 +55,11 @@ class ApacheLogParser:
         self.parser = apache_log_parser.make_parser(log_format)
 
     @classmethod
-    def from_path(cls, path:str, env:str=None, vhost:str=None) -> ApacheLogParser:
+    def from_path(cls, path, env=None, vhost=None):
         config = ApacheConfig(path, env_var_file=env)
         return cls(config, vhost)
 
-    def get_log_data(self, filter_func:function=None, skip_errors:bool=True) -> list:
+    def get_log_data(self, filter_func=None, skip_errors=True):
         log_data = []
         with open(self.log_file, "r") as file:
             for line in file:
