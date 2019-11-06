e07cd0626ed5f35204b7b51c83cce85c279b3822
RAG-6\Raphael <raphael.gerber@ace.ch>
2019-11-06 10:42:51 +0100

    -

--- check_system/FileSystemCheck.py
+++ check_system/FileSystemCheck.py
@@ -1,8 +1,6 @@
 
 import os
 
-from multiprocessing import Process
-
 
 class FSCheckException(Exception):
     pass
@@ -15,15 +13,20 @@ class FSCheck:
         self.store_path = store_path
         self.pid_file_name = "FSCheck.pid"
 
-    def exec(self, output_file):
+    def exec(self, output_file_path):
         # check if FSCheck is already running
         if self._is_running():
             # if found: abort
             raise FSCheckException("Process already running!")
 
         # else: fork process
-        proc = Process(target=self._internal_exec, args=(output_file))
-        proc.start()
+        pid = os.fork()
+
+        if pid == 0:
+            # child process -> execute actual fs check
+            self._internal_exec(output_file_path)
+        # proc = Process(target=self._internal_exec, args=(self, output_file))
+        # proc.start()
 
     def _is_running(self):
         # check if there's a pid file in the store_path
@@ -36,12 +39,11 @@ class FSCheck:
     def _delete_pid_file(self):
         os.remove(os.path.join(self.store_path, self.pid_file_name))
 
-    def _internal_exec(self, output_file):
+    def _internal_exec(self, output_file_path):
         # create pid file
         self._create_pid_file()
 
         # perform actual fs check here
-        
 
         # delete pid file
         self._delete_pid_file()
