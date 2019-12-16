21a9661d439ab8fcdbdbf1474ec301cd80184088
Raphael Gerber <raphael.gerber@ace.ch>
2019-12-16 10:49:17 +0100

    -

--- check_docker/check_docker_icc.py
+++ check_docker/check_docker_icc.py
@@ -38,17 +38,35 @@ def get_docker_networks():
     return networks.split("\n")[:-1]
 
 
-def inspect_docker_network(network):
-    cmd = f"docker network inspect {network}"
+def inspect_docker_networks(networks: list):
+    cmd = f"docker network inspect {' '.join(networks)}"
 
     try:
         config = subprocess.check_output(cmd, shell=True).decode("utf-8")
     except subprocess.CalledProcessError:
         print(
-            f"UNKNOWN: Unable to retrieve network configuration (net_id: {network})")
+            f"UNKNOWN: Unable to retrieve network configuration")
         sys.exit(UNKNOWN)
 
-    return json.loads(config)[0]
+    return json.loads(config)
+
+
+def get_default_bridge(networks: list):
+    default_bridge_option = "com.docker.network.bridge.default_bridge"
+
+    # retrieve network configurations for each network
+    network_configs = inspect_docker_networks(networks)
+
+    for network in network_configs:
+        # check if network options contain an entry for default bridge
+        options = network["Options"]
+
+        if default_bridge_option in options and options[default_bridge_option] == "true":
+            return network
+
+    # if this point is reached, then there was no default bridge configured
+    print("UNKNOWN: No default bridge configuration found...")
+    sys.exit(UNKNOWN)
 
 
 def main():
@@ -61,14 +79,39 @@ def main():
     if args.verbose:
         print(args)
 
+    # get a list of all configured docker networks
     docker_networks = get_docker_networks()
 
     if args.verbose:
         print(f"docker_networks: {docker_networks}")
 
-    for network in docker_networks:
-        network_config = inspect_docker_network(network)
-        print(network_config)
+    # get the default bridge
+    default_bridge = get_default_bridge(docker_networks)
+
+    # check if icc is enabled
+    enable_icc = default_bridge["Options"]["com.docker.network.bridge.enable_icc"]
+
+    if enable_icc != "true":
+        print(f"OK: Inter-container communication (icc) disabled")
+        sys.exit(OK)
+
+    # icc not disabled -> check if there are any containers on the default bridge
+
+    # get all containers that run on the default bridge (if any)
+    if "Containers" in default_bridge:
+        containers = default_bridge["Containers"]
+
+        if len(containers) > 1:
+            print(
+                f"CRITICAL: {len(containers)} containers running on the default bridge with icc enabled!")
+            sys.exit(CRITICAL)
+
+        if len(containers) == 1:
+            print(f"WARNING: 1 container running on the default bridge with icc enabled!")
+            sys.exit(WARNING)
+
+    print(f"OK: There are no containers running on the default bridge")
+    sys.exit(OK)
 
 
 if __name__ == "__main__":
